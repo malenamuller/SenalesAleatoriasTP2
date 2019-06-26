507cecfe3e7c39e0a697d7495a3512f3b3729ca8
malenamuller <mmuller@itba.edu.ar>
2019-06-26 15:03:14 -0300

    ej 1 item 1 matlab

--- EJ1/TP2_Ej1.m
+++ EJ1/TP2_Ej1.m
@@ -16,7 +16,7 @@ estimRxxNP = zeros(N,1); %contendrá el RxxNP para cada valor de k entre 0 y N-1
 for k = 0:N-1
     sum = 0;
     for i = 0:N-k-1
-        sum = sum + (S.x(i+1) * S.x(i+1+k))
+        sum = sum + (S.x(i+1) * S.x(i+1+k));
     end
     estimRxxNP(k+1) = (1/(N-k)) * sum; 
 end
@@ -26,10 +26,32 @@ estimRxxP = zeros(N,1); %contendrá el RxxP para cada valor de k entre 0 y N-1
 for k = 0:N-1
     sum = 0;
     for i = 0:N-k-1
-        sum = sum + (S.x(i+1) * S.x(i+1+k))
+        sum = sum + (S.x(i+1) * S.x(i+1+k));
     end
     estimRxxP(k+1) = (1/N) * sum; 
 end
 
 estimRxxNP
 estimRxxP
+
+estimrxxNP = estimRxxNP/max(estimRxxNP)%Normalizando Rxx
+estimrxxP = estimRxxP/max(estimRxxP)
+
+%rxx a partir del Rxx no polarizado
+figure(1)
+k = linspace(1,N,1);
+k = 0:1:N-1
+y = estimrxxNP(k+1);
+scatter(k,y)
+hold on
+plot(k,y)
+
+figure(2)
+%rxx a partir del Rxx polarizado
+k = linspace(1,N,1);
+k = 0:1:N-1
+y = estimrxxP(k+1);
+scatter(k,y)
+hold on
+plot(k,y)
+
