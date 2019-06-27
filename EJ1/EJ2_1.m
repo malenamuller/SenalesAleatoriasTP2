6f9ecaef1bfe22ae9f11048dca21a70e3bf42d4d
Marcelo Regueira <mregueira@itba.edu.ar>
2019-06-27 13:28:20 -0300

    3re

--- EJ1/EJ2_1.m
+++ EJ1/EJ2_1.m
@@ -9,11 +9,11 @@ S = load('Archivo_2.mat');
 %whos -file Archivo_2.mat
 
 N = 4096;
-
+length = 128;
 % Estimador no polarizado y polarizado
-RxxNP = zeros(N,1); %contendrá el RxxNP para cada valor de k entre 0 y N-1
-RxxP = zeros(N,1); %contendrá el RxxP para cada valor de k entre 0 y N-1
-for k = 0:N-1
+RxxNP = zeros(length,1); %contendrá el RxxNP para cada valor de k entre 0 y 127
+RxxP = zeros(length,1); %contendrá el RxxP para cada valor de k entre 0 y 127
+for k = 0:length-1
     sum = 0;
     for i = 0:N-k-1
         sum = sum + (S.x(i+1) * S.x(i+1+k));
@@ -30,5 +30,10 @@ clearvars k;
 rxxNP = RxxNP/RxxNP(1);
 rxxP = RxxP/RxxP(1);
 
-%% ITEM 2
+k = 0:1:length-1;
+figure;
+plot(k,rxxNP)
+figure;
+plot(k,rxxP)
 
+%% ITEM 2
