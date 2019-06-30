22bfcb1f2d4aa8af6f4454f78e4659d9db5372cd
jmestanza <jmestanza@itba.edu.ar>
2019-06-30 17:03:41 -0300

    probando

---
+++ EJ2/Ejercicio_2.m
@@ -0,0 +1,34 @@
+clear;
+clc;
+%leemos archivo (debe ser mono)
+file = 'whereIam8Khz.wav';
+info = audioinfo(file);
+[data,Fs] = audioread(file);
+t = 0:seconds(1/Fs):seconds(info.Duration);
+S = compand(data,255,max(data),'mu/compressor'); % esta es la señal
+snr = 30; %creo que esto esta en dB
+X_tot = awgn(S,snr); % X = S + N 
+Muestras = 160;
+n = 20;
+Iteraciones = cast(floor(length(S)/Muestras),'uint64'); 
+shat = double.empty;
+
+for l=0:Iteraciones-1
+    td = (1+l*Muestras):(Muestras+l*Muestras); % td es el tiempo entre muestras(tiene length = muestras)
+    Rxx = get_Rxx(X_tot(td),Muestras);
+    vector = Rxx(2:n+1)';
+    matrix =  toeplitz(Rxx(1:n));
+    h = matrix\vector; % es lo mismo inv(matrix)*vector
+    estimacion = conv(X_tot(td),h','same'); % computo la respuesta al filtro
+    shat = [shat estimacion']; % concateno las estimaciones en el vector Shat
+end
+subplot(3,1,1);
+plot(t(1:length(t)-1),S)
+title('Original')
+subplot(3,1,2);
+plot(t(1:length(X_tot)),X_tot)
+title('Con ruido')
+subplot(3,1,3);
+plot(t(1:length(shat)),shat)
+title('Estimación')
+sound(shat)
--- EJ2/get_Rxx.m
+++ EJ2/get_Rxx.m
@@ -1,14 +1,23 @@
-function [ Rxx ] = get_Rxx(x,N)
+%function [ Rxx ] = get_Rxx(x,Muestras)
+function [ acf ] = get_Rxx(xAux,muestras)
 %Esta función es para obtener el estimador de la autocorrelacion de X
 %Está en la página 567 del Shanmugan
-Rxx = double.empty ;
-Rxx_terms = double.empty;
-for k = 0:N-1
-    for i = 0:N-k-1
-        Rxx_terms = [Rxx_terms x(i+k+1)*x(i+1)]; % voy concatenando los terminos
+% Rxx = double.empty ;
+% Rxx_terms = double.empty;
+% for k = 0:Muestras-1
+%     for i = 0:Muestras-k-1
+%         Rxx_terms = [Rxx_terms x(i+k+1)*x(i+1)]; % voy concatenando los terminos
+%     end
+%     Rxx(k+1) = (1/(Muestras-k))*sum(Rxx_terms); % los sumo y divido por el k correspondiente
+% end
+acf = zeros(1,muestras-1);
+for j = 0.0:1.0:muestras-1
+    factor = (muestras-j);
+    cumsum = 0;
+    for k = 0.0:1.0:(muestras-j-1)
+        cumsum = cumsum + xAux(k+1)*xAux(k+j+1);
     end
-    Rxx(k+1) = (1/(N-k))*sum(Rxx_terms); % los sumo y divido por el k correspondiente
+    acf(j+1) = cumsum/factor;
 end
-
 end
 
