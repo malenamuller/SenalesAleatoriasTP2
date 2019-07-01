05a9b4d11e66db053e8ae6326d53163ba4efd5e5
jmestanza <jmestanza@itba.edu.ar>
2019-06-30 21:09:54 -0300

    update

--- EJ2/Ejercicio_2.m
+++ EJ2/Ejercicio_2.m
@@ -8,19 +8,28 @@ t = 0:seconds(1/Fs):seconds(info.Duration);
 S = compand(data,255,max(data),'mu/compressor'); % esta es la señal
 snr = 30; %creo que esto esta en dB
 X_tot = awgn(S,snr); % X = S + N 
-Muestras = 160;
-n = 11; % longitud de la rta impulsiva 
+Muestras = 160; 
 Iteraciones = cast(floor(length(S)/Muestras),'uint64'); 
 shat = double.empty;
 
 for l=0:Iteraciones-1
     td = (1+l*Muestras):(Muestras+l*Muestras); % td es el tiempo entre muestras(tiene length = muestras)
     Rxx = get_Rxx(X_tot(td),Muestras);
-    vector = Rxx(2:n+1)';
-    matrix =  toeplitz(Rxx(1:n));
-    h = matrix\vector; % es lo mismo inv(matrix)*vector
-    estimacion = conv(X_tot(td),h','same'); % computo la respuesta al filtro
-    shat = [shat estimacion']; % concateno las estimaciones en el vector Shat
+    Rss = get_Rxx(S(td),Muestras);
+    Sxx = fft(Rxx);
+    Sxs = fft(Rss);
+    k = 0:length(Sxx)-1;
+    alpha = -10;
+    shift = exp((1i*2*pi*k*alpha)/length(Sxx));
+    %(esto le aplica un (x(t+alpha)) alpha tiene que ser <0 para smoothing
+    H = (Sxs./Sxx).*(shift);
+    h = ifft(H);
+    h = real(h);
+    Entrada = fft(X_tot(td));
+    %estimacion = conv(X_tot(td),h','same'); % computo la respuesta al filtro
+    estimacion = real(ifft(H.*Entrada'));
+    
+    shat = [shat estimacion]; % concateno las estimaciones en el vector Shat
 end
 subplot(3,1,1);
 plot(t(1:length(t)-1),S)
